3d795c1da7c3ce392502965718fee81d07b4c4b3
Michael <mngraves@gmail.com>
2025-02-15 14:00:42 -0600

    Add automatic theme updates from GitHub and bump version to 1.0.2

--- functions.php
+++ functions.php
@@ -8,6 +8,39 @@
  * @since 1.0
  */
 
+ // Automatic theme updates from the GitHub repository
+add_filter('pre_set_site_transient_update_themes', 'automatic_GitHub_updates', 100, 1);
+function automatic_GitHub_updates($data) {
+  // Theme information
+  $theme   = get_stylesheet(); // Folder name of the current theme
+  $current = wp_get_theme()->get('Version'); // Get the version of the current theme
+  // GitHub information
+  $user = 'mngraves'; // The GitHub username hosting the repository
+  $repo = 'westernwitch'; // Repository name as it appears in the URL
+  // Get the latest release tag from the repository. The User-Agent header must be sent, as per
+  // GitHub's API documentation: https://developer.github.com/v3/#user-agent-required
+  $file = @json_decode(@file_get_contents('https://api.github.com/repos/'.$user.'/'.$repo.'/tags', false,
+      stream_context_create(['http' => ['header' => "User-Agent: ".$user."\r\n"]])
+  ));
+  if($file && sizeof($file) > 0) 
+  {
+	$tag = $file[0];
+	$tagVersion = filter_var($tag->name, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    // Only return a response if the new version number is higher than the current version
+    if($tagVersion > $current) {
+  	  $data->response[$theme] = array(
+	      'theme'       => $theme,
+	      // Strip the version number of any non-alpha characters (excluding the period)
+	      // This way you can still use tags like v1.1 or ver1.1 if desired
+	      'new_version' => $tagVersion,
+	      'url'         => 'https://github.com/'.$user.'/'.$repo,
+	      'package'     => 'https://github.com/'.$user.'/'.$repo . '/archive/refs/tags/' . $tag->name . '.zip'
+      );
+    }
+  }
+  return $data;
+}
+
 if ( ! function_exists( 'westernwitch_support' ) ) :
 
 	/**
